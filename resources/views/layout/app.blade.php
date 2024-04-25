0dfc8e6d604acfd8cdc0b55a3bf48cc7254249f9
andrearssi12 <andrea.rossi0507@gmail.com>
2024-04-25 14:59:33 +0700

    perbaiki layout dan install leaflet

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,16 +5,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>EMU</title>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+     integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+     crossorigin=""/>
+     <style>
+        #map { height: 500px; }
+     </style>
     @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
 <body>
-    <header>
-        @include('layout/navbar')
-    </header>
+    @include('layout/partials/navbar')
     <main>
         <div class="app">
             @yield('content')
         </div>
     </main>
+
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+     integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+     crossorigin=""></script>
+
+     @stack('scripts')
 </body>
 </html>
