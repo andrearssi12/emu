5b85c879b3678f503af6830aa828b35b5507d733
andrea <andrea.rossi0507@gmail.com>
2024-05-06 07:41:47 +0700

    memperbaiki navbar dan beranda

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,30 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>EMU</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>EMU - Eco Map UAD</title>
+    <link rel="shortcut icon" type="image/png/jpg" href="{{ asset('img/logo-warna.png') }}">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-     integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
-     crossorigin=""/>
-     <style>
-        #map { height: 500px; }
-     </style>
-    @vite(['resources/css/app.css','resources/js/app.js'])
+        integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+    <style>
+        #map {
+            height: 500px;
+        }
+    </style>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+
 <body>
     @include('layout/partials/navbar')
     <main>
-        <div class="app">
-            @yield('content')
-        </div>
+        @yield('content')
     </main>
 
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-     integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
-     crossorigin=""></script>
+        integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
 
-     @stack('scripts')
+    @stack('scripts')
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
-});
+    return view('beranda');
+})->name('beranda');
 Route::get('/login', function () {
     return view('login');
-});
+})->name('login');
