678262c7cb496eaa93fe2d7f41cc6a70f388a27f
andrea <andrea.rossi0507@gmail.com>
2024-08-16 01:32:20 +0700

    install bootstrap

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,9 @@
 import axios from 'axios';
+// Import our custom CSS
+import '../sass/app.scss';
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
