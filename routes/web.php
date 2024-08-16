038ad20100ecaed5c5514db97b04ff11b39128f5
andrea <andrea.rossi0507@gmail.com>
2024-08-17 04:31:34 +0700

    membuat layout awal

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $data = [
+        'pageTitle' => 'Beranda'
+    ];
+    return view('beranda', $data);
 });
